24a6daaa77da5dad46875423e03324a36bf20687
ediaze <ediaze@gmail.com>
2018-10-18 11:53:22 -0500

    Agregando Reporte a la Consola (#3)
    * setup protractor configuration
    
    * PR observations
    
    * Improve protractor test
    
    * Agregando Reporte a la Consola
    
    * Desactivar el manejador de promesas y Selenium server
    
    * 5.Chrome Headless
    
    * 6. Agregar Integración Continua
    
    * 6. Agregar Integración Continua-paso 2
    
    * 7. Agregando Análisis de Código Estático

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -2,9 +2,15 @@ import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 
 export const config: Config = {
+  SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  },
   framework: 'jasmine',
   specs: ['../test/*.spec.js'],
-  SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
