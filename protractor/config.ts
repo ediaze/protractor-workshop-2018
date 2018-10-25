33f749945f3b1bd776308a5ec5574c3a318226ed
ediaze <ediaze@gmail.com>
2018-10-25 07:41:22 -0500

    10. Use Page Object Model Pattern (#5)
    * setup protractor configuration
    
    * PR observations
    
    * Improve protractor test
    
    * Agregando Reporte a la Consola
    
    * Desactivar el manejador de promesas y Selenium server
    
    * 5.Chrome Headless
    
    * 6. Agregar Integración Continua
    
    * 6. Agregar Integración Continua-paso 2
    
    * 7. Agregando Análisis de Código Estático
    
    * 8. Depurando El Código
    
    * 8. Depurando El Código. Paso 2
    
    * 89 . CSS Selector
    
    * Add POM pattern
    
    * Modify page timeout

--- protractor/config.ts
+++ protractor/config.ts
@@ -2,12 +2,16 @@ import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 
 export const config: Config = {
+  SELENIUM_PROMISE_MANAGER: false,
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
-  SELENIUM_PROMISE_MANAGER: false,
-  getPageTimeout: 1000,
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
   }
 };
+
