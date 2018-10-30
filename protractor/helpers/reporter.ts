b80016714bae4960a6ab51497d3f7dfb7227227b
ediaze <ediaze@gmail.com>
2018-10-29 20:16:39 -0500

    Improve test (#7)
    * setup protractor configuration
    
    * PR observations
    
    * Improve protractor test
    
    * Agregando Reporte a la Consola
    
    * Desactivar el manejador de promesas y Selenium server
    
    * 5.Chrome Headless
    
    * 6. Agregar Integración Continua
    
    * 6. Agregar Integración Continua-paso 2
    
    * 7. Agregando Análisis de Código Estático
    
    * 8. Depurando El Código
    
    * 8. Depurando El Código. Paso 2
    
    * 89 . CSS Selector
    
    * Add POM pattern
    
    * Modify page timeout
    
    * 7. 13. Esperas Explicitas
    
    * 15. Separar prueba en diferentes describes. Ya lo tenÃia de un paso anterior
    
    * 16. Agregando Jasmine Awesome

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,9 +1,18 @@
 import { SpecReporter } from 'jasmine-spec-reporter';
 
+const { AwesomeReport } = require('jasmine-awesome-report');
+
+const config = {
+  fullPath: 'awesome-report',
+  fileName: 'report',
+  merge: true
+};
+
 export let reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: true
     }
   }));
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 };
--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -17,7 +17,7 @@ describe('Buy a t-shirt', () => {
     await browser.get('http://automationpractice.com/');
   });
 
-  describe('when want to buy a T shirt', () => {
+  describe('When want to buy a T shirt', () => {
     beforeAll(async () => {
       const menuContentPage: MenuContentPage = new MenuContentPage();
       const productListPage: ProductListPage = new ProductListPage();
@@ -30,19 +30,19 @@ describe('Buy a t-shirt', () => {
       await summaryStepPage.proceedToCheckout();
     });
 
-    describe('and login to the application', () => {
+    describe('And login to the application', () => {
       beforeAll(async () => {
         const signInStepPage: SignInStepPage = new SignInStepPage();
         await signInStepPage.login('aperdomobo@gmail.com', 'WorkshopProtractor');
       });
 
-      describe('and select default address', () => {
+      describe('And select default address', () => {
         beforeAll(async () => {
           const addressStepPage: AddressStepPage = new AddressStepPage();
           await addressStepPage.proceedToCheckout();
         });
 
-        describe('and pay to the bank', () => {
+        describe('And pay to the bank', () => {
           beforeAll(async () => {
             const shippingStepPage: ShippingStepPage = new ShippingStepPage();
             const paymentStepPage: PaymentStepPage = new PaymentStepPage();
@@ -53,7 +53,7 @@ describe('Buy a t-shirt', () => {
             await bankPaymentPage.confirmOrder();
           });
 
-          it('then the order should be completed', async () => {
+          it('Then the order should be completed', async () => {
             const orderResumePage: OrderSummaryPage = new OrderSummaryPage();
 
             await expect(orderResumePage.getOrderTitle())
