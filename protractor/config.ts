d4d98c8b3e9d86d4001eed2e604a91da36499273
Edwin Diaz <ediaze@psl.com.co>
2018-10-29 21:05:40 -0500

    17. Utilizando Capabilities para configurar Chrome

--- protractor/config.ts
+++ protractor/config.ts
@@ -2,17 +2,25 @@ import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 
 export const config: Config = {
-  SELENIUM_PROMISE_MANAGER: false,
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
+  SELENIUM_PROMISE_MANAGER: false,
   getPageTimeout: 30000,
+  capabilities: {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 2,
+    chromeOptions: {
+      args: ['disable-infobars=true', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
   },
   onPrepare: () => {
+    reporter();
     browser.ignoreSynchronization = true;
     browser.manage().timeouts().implicitlyWait(0);
-    reporter();
   }
 };
-
